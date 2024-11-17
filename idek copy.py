97c345c8f1aa07c695f8b8a175b4b000cb4dd43f
Mehak Sahjwani <mehaksahjwani@gmail.com>
2024-11-16 23:32:41 -0700

    WITH DATABASE

--- idek copy.py
+++ idek copy.py
@@ -1,4 +1,3 @@
-
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
 from brainflow.data_filter import DataFilter, FilterTypes
 import numpy as np
@@ -6,99 +5,105 @@ import matplotlib.pyplot as plt
 import time
 from brainflow.data_filter import WindowOperations
 from datetime import datetime
+from routes import WhileTrue
 
 
-params = BrainFlowInputParams()
-params.serial_port = 'COM6' #Change this depending on your device and OS
-board_id = 39 #Change this depending on your device
-beta=[]
-theta=[]
+def muse():
+    params = BrainFlowInputParams()
+    params.serial_port = 'COM6' #Change this depending on your device and OS
+    board_id = 39 #Change this depending on your device
+    beta=[]
+    theta=[]
+    countMuse =0
 
 
-#Prepares the board for reading data
-try:
-    board_id = 39
-    board = BoardShim(board_id, params)
-    board.prepare_session()
-    print("Successfully prepared physical board.")
-except Exception as e:
-    print(e)
-    #If the device cannot be found or is being used elsewhere, creates a synthetic board instead
-    print("Device could not be found or is being used by another program, creating synthetic board.")
-    board_id = BoardIds.SYNTHETIC_BOARD
-    board = BoardShim(board_id, params)
-    board.prepare_session()
+    #Prepares the board for reading data
+    try:
+        board_id = 39
+        board = BoardShim(board_id, params)
+        board.prepare_session()
+        print("Successfully prepared physical board.")
+    except Exception as e:
+        print(e)
+        #If the device cannot be found or is being used elsewhere, creates a synthetic board instead
+        print("Device could not be found or is being used by another program, creating synthetic board.")
+        board_id = BoardIds.SYNTHETIC_BOARD
+        board = BoardShim(board_id, params)
+        board.prepare_session()
 
 
-print("Starting Stream")
-board.start_stream()
-eeg_channels = board.get_eeg_channels(board_id)
-sampling_rate = BoardShim.get_sampling_rate(board_id)
-time.sleep(2)
+    print("Starting Stream")
+    board.start_stream()
+    eeg_channels = board.get_eeg_channels(board_id)
+    sampling_rate = BoardShim.get_sampling_rate(board_id)
+    time.sleep(2)
 
-for i in range(125):
-    time.sleep(0.5)
-    current_data = board.get_current_board_data(128)
-    current_eeg_data = current_data[eeg_channels] 
-    theta_total, alpha_total, beta_total = 0, 0, 0
+    while (WhileTrue):
+        time.sleep(0.5)
+        current_data = board.get_current_board_data(128)
+        current_eeg_data = current_data[eeg_channels] 
+        theta_total, beta_total = 0, 0
 
-    for channel_data in current_eeg_data:
-        if len(channel_data) > 0:
+        for channel_data in current_eeg_data:
+            if len(channel_data) > 0:
 
-        # Apply filters to clean the signal
-            DataFilter.perform_lowpass(channel_data, BoardShim.get_sampling_rate(board_id), 40.0, 5,
-                                    FilterTypes.CHEBYSHEV_TYPE_1, 0.5)
-            DataFilter.perform_highpass(channel_data, BoardShim.get_sampling_rate(board_id), 2.0, 4,
+            # Apply filters to clean the signal
+                DataFilter.perform_lowpass(channel_data, BoardShim.get_sampling_rate(board_id), 40.0, 5,
                                         FilterTypes.CHEBYSHEV_TYPE_1, 0.5)
-        # Compute PSD
-            psd = DataFilter.get_psd(channel_data, sampling_rate, WindowOperations.HANNING)
-            #print(psd)
-        # Extract band powers
-            theta_power = DataFilter.get_band_power(psd, 4, 8)
-            beta_power = DataFilter.get_band_power(psd, 13, 32)
+                DataFilter.perform_highpass(channel_data, BoardShim.get_sampling_rate(board_id), 2.0, 4,
+                                            FilterTypes.CHEBYSHEV_TYPE_1, 0.5)
+            # Compute PSD
+                psd = DataFilter.get_psd(channel_data, sampling_rate, WindowOperations.HANNING)
+                #print(psd)
+            # Extract band powers
+                theta_power = DataFilter.get_band_power(psd, 4, 8)
+                beta_power = DataFilter.get_band_power(psd, 13, 32)
 
-            theta_total += theta_power
-            beta_total += beta_power
-    if i < 50:
-        beta.append(beta_total)
-        theta.append(theta_total)
-    if i == 50:
-        beta_threshold = (sum(beta)/50)
-        theta_threshold = (sum(theta)/50)
-    
-    
+                theta_total += theta_power
+                beta_total += beta_power
+        if countMuse < 50:
+            beta.append(beta_total)
+            theta.append(theta_total)
+        if countMuse == 50:
+            beta = np.array(beta)
+            #mean_beta = np.mean(beta, axis=0)
+            Q1_beta = np.percentile(beta, 25, method ='midpoint')
+            Q3_beta = np.percentile(beta, 75, method ='midpoint')
+            IQR_beta = Q3_beta - Q1_beta
+            lower_bound_beta = Q1_beta - 1.5 * IQR_beta
+            upper_bound_beta = Q3_beta + 1.5 * IQR_beta
+            beta = [x for x in beta if x > lower_bound_beta and x < upper_bound_beta]
+            beta_threshold = np.mean(beta, axis = 0)
+            std_beta = np.std(beta)
 
-    print(i)
-    print(beta_total,theta_total)
-    beta.append(beta_total)
-    theta.append(theta_total)
-
-data = board.get_board_data()
-print(data.shape)
-print("Ending stream")
-board.stop_stream()
-board.release_session()
-
-print(beta)
-print("Concentrate:", beta[0:49], (sum(beta[0:49])/50))
-print("Yap:", beta[50:74], (sum(beta[50:74])/25))
-print("Look away:", beta[75:99], (sum(beta[75:99])/25))
-print("Zoning:", beta[100:124], (sum(beta[100:124])/25))
-
-print(theta)
-print("Awake:", theta[0:49], (sum(theta[0:49])/50))
-print("closed:", theta[50:74], (sum(theta[50:74])/25))
-print("head tilt:", theta[75:99], (sum(theta[75:99])/25))
-print("Awake:", theta[100:124], (sum(theta[100:124])/25))
+            theta = np.array(theta)
+            #mean_theta = np.mean(theta, axis = 0)
+            Q1_theta = np.percentile(theta, 25, method ='midpoint')
+            Q3_theta = np.percentile(theta, 75, method ='midpoint')
+            IQR_theta = Q3_theta - Q1_theta
+            lower_bound_theta = Q1_theta - 1.5 * IQR_theta
+            upper_bound_theta = Q3_theta + 1.5 * IQR_theta
+            theta = [x for x in theta if x > lower_bound_theta and x < upper_bound_theta]
+            theta_threshold = np.mean(theta, axis = 0)
+            std_theta = np.std(theta)
+        
+        #beta.append(beta_total)
+        #theta.append(theta_total)
+        countMuse +=1
+        concentration = beta_total
+        fatigue = theta_total
+        if concentration > beta_threshold + std_beta :
+            concentrationCall()
+        if fatigue > theta_threshold + std_theta:
+            fatigueCall()
 
 
-eeg_channels = board.get_eeg_channels(board_id)
-eeg_data = data[eeg_channels]
-plt.plot(np.arange(eeg_data.shape[1]), eeg_data[0])
 
 
-print(eeg_data.shape)
-DataFilter.write_file(eeg_data, 'eeg_data_test.csv', 'w') #Writes into a csv file in the current directory
+    data = board.get_board_data()
+    print(data.shape)
+    print("Ending stream")
+    board.stop_stream()
+    board.release_session()
 
-restored_data = DataFilter.read_file('eeg_data_test.csv') #Reads file back
-print(restored_data.shape)
+  
